185fbd307c510da4c2e16d6b119de7fd77d238d5
fisker <lionkay@gmail.com>
2017-08-24 10:34:15 +0800

    update publish script

--- npm-publish.bat
+++ npm-publish.bat
@@ -1,5 +1,4 @@
 call gulp test
 call gulp release
-call npm config set registry https://registry.npmjs.org/
-call npm publish
-call npm config set registry https://registry.npm.taobao.org/
+call npm --registry=https://registry.npmjs.org/ publish
+pause
